51fdd834242b6da34848c8954d87fcc012e5aa5e
Old Engineer <oldengineer1911@gmail.com>
2020-01-17 12:58:23 +0100

    Add possibility to change colours
    It should be possible to change background-color for each div by javascript.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
 function doChange() {
 alert('clicked button from js file');
 }
+
+function changeColor() {
+var divElement1 = document.getElementById("div1");
+var divElement2 = document.getElementById("div2");
+var divElement3 = document.getElementById("div3");
+var divElement4 = document.getElementById("div4");
+var divElement5 = document.getElementById("div5");
+
+divElement1.className = green;
+divElement2.className = blue;
+divElement3.className = red;
+divElement4.className = cyan;
+divElement5.className = yellow;
+}
