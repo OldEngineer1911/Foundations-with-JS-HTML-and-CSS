85e81e4ec606b1a74143276a6f3422a85c0d5977
Old Engineer <oldengineer1911@gmail.com>
2020-01-20 08:31:21 +0100

    Add next div ID.
    Its because I forgot to add it.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function doChange() {
-alert('clicked button from js file');
+alert('Clicked button from js file.');
 }
 
 function changeColor() {
@@ -8,12 +8,14 @@ var divElement2 = document.getElementById("div2");
 var divElement3 = document.getElementById("div3");
 var divElement4 = document.getElementById("div4");
 var divElement5 = document.getElementById("div5");
+var divElement5 = document.getElementById("div6");
 
 divElement1.className = green;
 divElement2.className = blue;
 divElement3.className = red;
 divElement4.className = cyan;
 divElement5.className = yellow;
+divElement6.className = green;
 }
 
 window.addEventListener('load', function () {
