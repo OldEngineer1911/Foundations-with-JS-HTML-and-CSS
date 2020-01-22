1dd6591ceba9aac199b5478e8dea655a35dfd050
Old Engineer <oldengineer1911@gmail.com>
2020-01-22 13:32:21 +0100

    Add possibility to dynamically change color of text and value of text.

--- javascript.js
+++ javascript.js
@@ -3,16 +3,26 @@ alert('Clicked button from js file.');
 }
 
 function changeColor() {
-var divElement1 = document.getElementById("div1");
-var divElement2 = document.getElementById("div2");
-var divElement3 = document.getElementById("div3");
-var divElement4 = document.getElementById("div4");
-var divElement5 = document.getElementById("div5");
-var divElement6 = document.getElementById("div6");
-divElement1.className = "green";
-divElement2.className = "blue";
-divElement3.className = "red";
-divElement4.className = "cyan";
-divElement5.className = "yellow";
-divElement6.className = "green";
+var element1 = document.getElementById("div1");
+var element2 = document.getElementById("div2");
+var element3 = document.getElementById("div3");
+var element4 = document.getElementById("div4");
+var element5 = document.getElementById("div5");
+var element6 = document.getElementById("div6");
+element1.className = "green";
+element2.className = "blue";
+element3.className = "red";
+element4.className = "cyan";
+element5.className = "yellow";
+element6.className = "green";
+}
+
+function changeTextColor() {
+  var element = document.getElementById("changeTextColor");
+  element.style="color:red";
+}
+
+function changeButtonText() {
+  var element = document.getElementById("changeTextValue");
+  element.innerHTML="This is new text, function is correctly implemented."
 }
