f8cd4952dc0d1456aaa29db45f32b0b7fd750f1f
Old Engineer <oldengineer1911@gmail.com>
2020-01-26 09:56:32 +0100

    Reformat code according to JS-HTML-CSS Formatter plugin

--- javascript.js
+++ javascript.js
@@ -1,28 +1,28 @@
 function doChange() {
-alert('Clicked button from js file.');
+    alert('Clicked button from js file.');
 }
 
 function changeColor() {
-var element1 = document.getElementById("div1");
-var element2 = document.getElementById("div2");
-var element3 = document.getElementById("div3");
-var element4 = document.getElementById("div4");
-var element5 = document.getElementById("div5");
-var element6 = document.getElementById("div6");
-element1.className = "green";
-element2.className = "blue";
-element3.className = "red";
-element4.className = "cyan";
-element5.className = "yellow";
-element6.className = "green";
+    var element1 = document.getElementById("div1");
+    var element2 = document.getElementById("div2");
+    var element3 = document.getElementById("div3");
+    var element4 = document.getElementById("div4");
+    var element5 = document.getElementById("div5");
+    var element6 = document.getElementById("div6");
+    element1.className = "green";
+    element2.className = "blue";
+    element3.className = "red";
+    element4.className = "cyan";
+    element5.className = "yellow";
+    element6.className = "green";
 }
 
 function changeTextColor() {
-  var element = document.getElementById("changeTextColor");
-  element.style="color:red";
+    var element = document.getElementById("changeTextColor");
+    element.style = "color:red";
 }
 
 function changeButtonText() {
-  var element = document.getElementById("changeTextValue");
-  element.innerHTML="This is new text, function is correctly implemented."
+    var element = document.getElementById("changeTextValue");
+    element.innerHTML = "This is new text, function is correctly implemented."
 }
