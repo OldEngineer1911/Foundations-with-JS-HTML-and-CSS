a889c145fc028e5dd24f6a456d20ec6fa60c6bd1
Old Engineer <oldengineer1911@gmail.com>
2020-02-02 10:50:55 +0100

    Change div to canvas
    In the HTML pane, change each div element to a canvas element.
    In the HTML and JS panes, remove all but the Change Color button and any related functions.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-function doChange() {
-    alert('What a question.. In js file of courser :) ');
-}
-
 function changeColor() {
     var element1 = document.getElementById("div1");
     var element2 = document.getElementById("div2");
@@ -15,36 +11,4 @@ function changeColor() {
     element4.className = "cyan";
     element5.className = "yellow";
     element6.className = "green";
-}
-
-function changeTextColor() {
-    var element = document.getElementById("changeTextColor");
-    element.style = "color:red";
-}
-
-function changeButtonText() {
-    var element = document.getElementById("changeTextValue");
-    element.innerHTML = "This is new text, function is correctly implemented."
-}
-
-function doLimeDiv() {
-    var element = document.getElementById("divLime");
-    element.style.backgroundColor = "lime";
-}
-
-function doLime() {
-    var element = document.getElementById("d1");
-    element.style.backgroundColor = "lime";
-}
-
-
-function doYellow() {
-    var element = document.getElementById("d1");
-    element.style.backgroundColor = "white";
-
-    var context = element.getContext("2d");
-    context.fillStyle = "yellow";
-    context.fillRectangle(10, 10, 40, 40);
-    context.font = ("30px Arial");
-    context.fillText("Hello world", 10, 80);
 }
