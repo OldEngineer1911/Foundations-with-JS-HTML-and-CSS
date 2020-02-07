149e0d923b97a3789b3afce4726eb7df46466478
Old Engineer <oldengineer1911@gmail.com>
2020-02-07 21:15:27 +0100

    Use canvas instead of div

--- javascript.js
+++ javascript.js
@@ -4,3 +4,30 @@ function changeColor() {
     element1.className = "fuchsiaback";
     element2.className = "orangeback";
 }
+
+function changeColorToRed() {
+    var element = document.getElementById("canvas7");
+    element.style.backgroundColor = "red";
+}
+
+function changeColorToBlue() {
+    var element = document.getElementById("canvas8");
+    element.style.backgroundColor = "blue";
+
+    var context = element.getContext("2d");
+    context.fillStyle = "yellow";
+    context.fillRect(10, 10, 60, 60);
+    context.fillRect(80, 10, 60, 60);
+
+    context.fillStyle = "black";
+    context.font = "20px Arial";
+    context.fillText("Hello", 15, 45);
+}
+
+function clearBlueCanvas() {
+    var element = document.getElementById("canvas8");
+    var context = element.getContext("2d");
+
+    context.clearRect(0, 0, element.width, element.height);
+    element.style.backgroundColor = "blue";
+}
